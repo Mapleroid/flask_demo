9faee40a7a3e9adb446690c6e207ddc286147eb5
Weifeng Zhao <mapleroid@qq.com>
2017-05-18 08:56:11 +0800

    测试了flask-restless对数据库操作的GET、POST、DELETE、PUT方法

--- restless/request.py
+++ restless/request.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+
 import requests
 import uuid
 import json
@@ -6,6 +7,23 @@ import json
 headers = {'Content-type': 'application/json',
                    'Accept': 'text/plain'}
 
-requests.post("%s/v1/fractal" % "http://localhost",
-              json.dumps({'uuid':str(uuid.uuid4()),'username':'test'}),headers=headers)
 
+# add a item
+#requests.post("%s/v1/fractal" % "http://localhost:5000",
+#              json.dumps({'uuid':str(uuid.uuid4()),'username':'test2'}),headers=headers)
+
+
+# update a item
+#requests.put("%s/v1/fractal/%s" % ("http://localhost:5000",'f0e3480e-60f9-42e9-9769-cd7dd62dd138'),
+#              json.dumps({'username':'test3'}),headers=headers)
+
+# delete a item
+#requests.delete("%s/v1/fractal/%s" % ("http://localhost:5000",'f0e3480e-60f9-42e9-9769-cd7dd62dd138'),
+#                 headers=headers)
+
+# get a item
+#result = requests.get("%s/v1/fractal/%s" % ("http://localhost:5000",'a2746f2b-c905-4389-a8e0-e758749f0b35'),
+#                 headers=headers)
+
+#if result.status_code == 200:
+#    print result.text
